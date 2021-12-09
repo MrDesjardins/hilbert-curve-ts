d10b004cab500132a139f984036327ec02619f21
Patrick Desjardins <pdesjardins@netflix.com>
2021-12-09 14:26:17 -0800

    Benchmark

---
+++ benchmarks/benchmarks.ts
@@ -0,0 +1,48 @@
+const Benchmark = require("benchmark");
+const suite = new Benchmark.Suite();
+
+const { HilbertAlgorithm } = require("../dist/lib/es5/hilbertAlgorithm");
+
+// add tests
+suite
+  .add("Hilbert Order 2", function () {
+    const h = new HilbertAlgorithm(2);
+    h.indexToPoint(0);
+  })
+  // .add("Hilbert Order 3", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 4", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 5", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 6", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 7", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 8", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 9", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // .add("Hilbert Order 10", function () {
+  //   const h = new HilbertAlgorithm(3);
+  //   h.indexToPoint(0);
+  // })
+  // add listeners
+  .on("cycle", function (event: any) {
+    console.log(String(event.target));
+  })
+  .run();
